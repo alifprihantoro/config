332f6b2bea046e6048fd7a536b58ac105b2b8552
alifmuryp <alifmuryp@gmail.com>
2021-07-02 06:41:28 +0700

    fixed ls

--- bash/alias/oh_my_bash/ls.aliases.sh
+++ bash/alias/oh_my_bash/ls.aliases.sh
@@ -4,9 +4,9 @@
 # Directory Listing aliases
 alias dir='ls -hFx'
 alias l.='ls -d .* --color=tty' # short listing, only hidden files - .*
-alias l='ls -lathF'             # long, sort by newest to oldest
+alias ll='ls -lathF'             # long, sort by newest to oldest
 alias L='ls -latrhF'            # long, sort by oldest to newest
-alias la='ls -Al'               # show hidden files
+alias lA='ls -Al'               # show hidden files
 alias lc='ls -lcr'              # sort by change time
 alias lk='ls -lSr'              # sort by size
 alias lh='ls -lSrh'             # sort by size human readable

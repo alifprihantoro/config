142f00450dbf0a9e22c534fbf8b8c5997fa06baa
alifmuryp <alifmuryp@gmail.com>
2021-07-08 15:49:55 +0700

    Update on Thu Jul  8 15:49:54 WIB 2021 with change : M  alias/app/vimwiki.sh

--- bash/alias/app/vimwiki.sh
+++ bash/alias/app/vimwiki.sh
@@ -1,12 +1,24 @@
-alias wl='cd ~/storage/external-1/wiki
+# dir
+wl=$de/wiki
+w1=$wl/development
+w2=$wl/docs
+w3=$wl/private
+
+alias dwl='cd wl'
+alias dw1='cd w1'
+alias dw2='cd w2'
+alias dw3='cd w3'
+
+# use 
+alias wl='wl
 v index.md
 '
-alias w1='cd ~/storage/external-1/wiki/development
+alias w1='cd w1
 v index.md
 '
-alias w2='cd ~/storage/external-1/wiki/docs
+alias w2='cd w2
 v index.md
 '
-alias w3='cd ~/storage/external-1/wiki/privat
+alias w3='cd w3
 v index.md
 '

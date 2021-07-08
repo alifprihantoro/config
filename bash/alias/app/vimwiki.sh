3986523f2a3f30a8944d10fcf486528dc44f2f9a
alifmuryp <alifmuryp@gmail.com>
2021-07-08 15:39:46 +0700

    add alias wiki vim

---
+++ bash/alias/app/vimwiki.sh
@@ -0,0 +1,9 @@
+alias w1='cd ~/storage/external-1/wiki/development
+v index.md
+'
+alias w2='cd ~/storage/external-1/wiki/docs
+v index.md
+'
+alias w3='cd ~/storage/external-1/wiki/privat
+v index.md
+'

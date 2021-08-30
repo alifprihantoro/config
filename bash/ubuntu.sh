a5a064b8e79de45eda662a18deea03b28e9e4542
alifmuryp <alifmuryp@gmail.com>
2021-08-30 11:16:24 +0700

    ubuntu lupa commit

--- bash/ubuntu.sh
+++ bash/ubuntu.sh
@@ -1,5 +1,5 @@
-## source it from ~/.bashrc or ~/.bash_profile ##
-# source ~/../usr/etc/profile.d/bash_completion.sh
+## source it from /data/data/com.termux/files/home/.bashrc or /data/data/com.termux/files/home/.bash_profile ##
+# source /data/data/com.termux/files/home/../usr/etc/profile.d/bash_completion.sh
 default_bash_dir=/data/data/com.termux/files/home/.config/bash
 ## my alias
 source $default_bash_dir/theme.sh
@@ -13,3 +13,29 @@ source $default_bash_dir/alias/init.sh
 for sumber_completion in $default_bash_dir/completions/*.sh; do 
   source $sumber_completion
 done
+
+de=/data/data/com.termux/files/home/storage/external-1
+zzz=/data/data/com.termux/files/home/zzz
+dc=/data/data/com.termux/files/home/.config
+db=$dc/bash
+dv=$dc/nvim
+ssh=$de/.ssh
+
+# web muryp
+dw=$zzz/web # dir web
+dm=$dw/muryp # default dir muryp
+dmd=$dw/deploy-muryp/ # deploy dir
+dmsi=$dm/static/img/public # img
+dmss=$dm/static/script/public # script
+dmsp=$dm/static/pages-api # pages api
+dmsb=$dm/static/blog-api # blog api
+dmt=$dm/themes/muryp # theme
+dmtp=$dmt/layouts/partials # partial
+dmtpmh=$dmtp/main/head/ # head
+dmtps=$dmtp/script # script theme
+dmc=$dm/content # content
+dmcb=$dm/content/blog # content blog
+dmcn=$dm/content/note # content lms
+dmk=$dm/config/_default/ # config
+
+

47602de4897a16605977142506f4d03df3a1e925
alifmuryp <alifmuryp@gmail.com>
2021-10-09 14:31:54 +0700

    dir ubuntu

--- bash/ubuntu/var.sh
+++ bash/ubuntu/var.sh
@@ -1,8 +1,8 @@
-de=~/storage/external-1
-zzz=~/zzz
+dt=/data/data/com.termux/files/home
 dc=~/.config
 db=$dc/bash
 dv=$dc/nvim
 ssh=$de/.ssh
+zzz=$dt/zzz
 
 source /data/data/com.termux/files/home/.config/bash/dir/main.sh

e966b8c1ed2a0afccb25d0b2bd3e8ecc9886241e
alifmuryp <alifmuryp@gmail.com>
2022-02-26 09:00:15 +0700

    fix gsv

--- git/alias/commit.sh
+++ git/alias/commit.sh
@@ -1,27 +1,31 @@
 # git commit
 alias gcm='git commit -am'
-# with comment
-alias gsv='
+
+## with comment
+# alias gsv='
+# ga .
+# statusCommit=$(git status -s)
+# while [ "$statusCommit" != "" ]
+# do
+# echo "kamu juga belum melakukan commit pada : \\n $statusCommit"
+# read -p "Your Message : " msg
+# gcm "$msg"
+# break
+# done'
+
+function gsv(){
 ga .
 statusCommit=$(git status -s)
 while [ "$statusCommit" != "" ]
 do 
-echo "kamu juga belum melakukan commit pada : \\n $statusCommit"
-read -p "Your Message : " msg
-gcm "$msg"
-break
-done'
-gsv2(){
-ga .
-statusCommit=$(git status -s)
-while [ "$statusCommit" != "" ]
-do 
-echo "kamu melakukan commit pada : \\n $statusCommit"
-echo "Your Message : $1" 
-gcm "$1"
-break
+  echo "kamu melakukan commit pada : \\n $statusCommit"
+  echo "Your Message : $1" 
+  gcm "$1"
+
+  break
 done
 }
+
 # auto commit
 alias gsva='
 ga .

ae82ddf4a6c101db13a55ed2b5d4963d46c55849
alifprihantoro <alifmuryp@gmail.com>
2022-03-13 19:43:45 +0700

    fixing app.zsh

---
+++ app.zsh
@@ -0,0 +1,8 @@
+dr=~/
+cd $dr.myconf/
+echo "#_________start git" >> $dr.myconf/install.zsh
+for sumber_dir in $(find **/*.sh -type f -not -path 'bash/*' ! -path "*/ubuntu*" ! -path "web*" ! -path "*/oh_my_bash/*"); do
+  echo "source $dr.myconf/$sumber_dir" >> $dr.myconf/install.zsh
+done
+
+echo "#_________start git" >> $dr.myconf/install.zsh

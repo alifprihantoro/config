b8324c6cc2e696bd986ae876b9db80942b0a0e37
alifprihantoro <alifmuryp@gmail.com>
2022-06-08 09:47:40 +0700

    upt dir

--- dir/variable.sh
+++ dir/variable.sh
@@ -4,6 +4,7 @@ dc=$dr/.myconf
 # db=$dc/bash
 dssh=$dr.ssh
 dw=$dr/web
+dm=$dw/muryp-new
 dvp=$dr.config/plugin
 dv=$dvp/nvim-conf
 
@@ -14,5 +15,6 @@ alias db='cd $db'
 alias dv='cd $dv'
 alias dssh='cd $dssh'
 alias dw='cd $dw'
+alias dm='cd $dm'
 alias dvc='cd $dvc'
 alias dvp='cd $dvp'
